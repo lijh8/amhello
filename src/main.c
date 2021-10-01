91ee224f0ec4f5fd5b09f42bf837a9a454571073
Jack Li <ljh_app@qq.com>
2021-10-01 10:45:39 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include <assert.h>
 #include "say.h"
 
-// #ifdef NDEBUG
-// #include "aaa.h"
-// #else
-// #include "bbb.h"
-// #endif
+#ifdef NDEBUG
+#include "aaa.h"
+#else
+#include "bbb.h"
+#endif
 
 int main(int argc, char *argv[]){
     say_hello();
